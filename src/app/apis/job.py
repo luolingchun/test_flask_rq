2130fb40bae54f90e1089f2d43c2649489bdd3be
luolingchun <luolingchun@mapscloud.cn>
2019-07-26 15:29:26 +0800

    update job API

--- src/app/apis/job.py
+++ src/app/apis/job.py
@@ -3,7 +3,7 @@
 # @Author  : llc
 # @File    : job.py
 import traceback
-
+from flask import request
 from flask_restplus import Resource, Namespace, reqparse
 from rq.exceptions import NoSuchJobError
 from rq.job import Job, requeue_job
@@ -19,40 +19,25 @@ __version__ = 'v1'
 ns = Namespace(f'{__version__}/jobs', description='任务管理 API接口')
 
 parser1 = reqparse.RequestParser(bundle_errors=True)
-parser1.add_argument(name='queue_name', type=str, choices=(Config.RQ_QUEUES), location='path', required=True,
+parser1.add_argument(name='queue_name', type=str, choices=(Config.RQ_QUEUES_ALL), location='args', required=True,
+                     help=QUEUE_ALL_HELP)
+parser1.add_argument(name='job_status', type=str, choices=(Config.RQ_JOB_STATUS), location='args', required=True,
+                     help=JOB_STATUS_HELP)
+
+parser2 = reqparse.RequestParser(bundle_errors=True)
+parser2.add_argument(name='queue_name', type=str, choices=(Config.RQ_QUEUES), location='form', required=True,
                      help=QUEUE_HELP)
 
 
-@ns.route('/<queue_name>')
-@ns.expect(parser1)
+@ns.route('')
 class NewJobAPI(Resource):
-    @ns.doc(id='post job', description='添加任务')
-    def post(self, queue_name):
-        '''添加任务'''
-        if queue_name not in Config.RQ_QUEUES:
-            return {'code': StatesCode.QUEUE_NOT_EXIST, 'message': '任务队列不存在！'}
-        job_id = str(uuid4())
-        job = queue_dict[queue_name].enqueue(add, args=(1, 2, job_id), job_id=job_id,
-                                             job_timeout=Config.RQ_DEFAULT_TIMEOUT,)
-                                             # depends_on='ccebebb4-2f4f-4829-b487-683178d90170')
-        # job = queue_dict[queue_name].enqueue(add, args=(1, '2'), job_timeout=Config.RQ_DEFAULT_TIMEOUT)  # 错误任务
-        job_id = job.get_id()
-        return {'code': 0, 'data': job_id}
-
-
-parser2 = reqparse.RequestParser(bundle_errors=True)
-parser2.add_argument(name='job_status', type=str, choices=(Config.RQ_JOB_STATUS), location='path', required=True,
-                     help=JOB_STATUS_HELP)
-parser2.add_argument(name='queue_name', type=str, choices=(Config.RQ_QUEUES_ALL), location='path', required=True,
-                     help=QUEUE_ALL_HELP)
-
-
-@ns.route('/<job_status>/<queue_name>')
-@ns.expect(parser2)
-class QueuedJobListAPI(Resource):
     @ns.doc(id='get job list', description='根据任务状态、队列名称获取任务列表')
-    def get(self, job_status, queue_name):
+    @ns.expect(parser1)
+    def get(self):
         """获取任务列表"""
+        args = request.args
+        job_status = args.get('job_status')
+        queue_name = args.get('queue_name')
         if job_status not in Config.RQ_JOB_STATUS:
             return {'code': StatesCode.JOB_STATUS_NO_EXIST, 'message': '任务状态不存在！'}
         if queue_name not in Config.RQ_QUEUES_ALL:
@@ -98,6 +83,20 @@ class QueuedJobListAPI(Resource):
                 job_list = deferred_job_registry.get_job_ids()
         return {'code': StatesCode.SUCCESS, 'data': job_list}
 
+    @ns.doc(id='post job', description='添加任务')
+    @ns.expect(parser2)
+    def post(self):
+        '''添加任务'''
+        form = request.form
+        queue_name = form.get('queue_name')
+        if queue_name not in Config.RQ_QUEUES:
+            return {'code': StatesCode.QUEUE_NOT_EXIST, 'message': '任务队列不存在！'}
+        job_id = str(uuid4())
+        job = queue_dict[queue_name].enqueue(add, args=(1, 2, job_id), job_id=job_id,
+                                             job_timeout=Config.RQ_DEFAULT_TIMEOUT, )
+        # job = queue_dict[queue_name].enqueue(add, args=(1, '2'), job_timeout=Config.RQ_DEFAULT_TIMEOUT)  # 错误任务
+        return {'code': 0, 'data': job.get_id()}
+
 
 parser3 = reqparse.RequestParser(bundle_errors=True)
 parser3.add_argument(name='job_id', type=str, location='path', required=True, help='任务id')
@@ -133,6 +132,19 @@ class JobAPI(Resource):
         }
         return {'code': 0, 'data': info}
 
+    @ns.doc(id='requeue job', description='重新开始任务')
+    def put(self, job_id):
+        '''重新开始任务'''
+        try:
+            requeue_job(job_id, connection=rq2.connection)
+        except NoSuchJobError:
+            return {'code': StatesCode.JOB_NOT_EXIST, 'message': '任务不存在！'}
+        except Exception as e:
+            traceback.print_exc()
+            return {'code': StatesCode.UNKNOWN_ERROR, 'message': f'位置错误：{str(e)}'}
+
+        return {'code': 0, 'data': job_id}
+
     @ns.doc(id='delete job', description='删除任务')
     def delete(self, job_id):
         '''删除任务'''
@@ -144,21 +156,4 @@ class JobAPI(Resource):
             return {'code': StatesCode.UNKNOWN_ERROR, 'message': str(e)}
         job.cancel()
         # job.delete()
-        return {'code': 0, 'data': job_id}
-
-
-@ns.route('/requeue/<job_id>')
-@ns.expect(parser3)
-class RequeueJobAPI(Resource):
-    @ns.doc(id='requeue job', description='重新开始任务')
-    def post(self, job_id):
-        '''重新开始任务'''
-        try:
-            requeue_job(job_id, connection=rq2.connection)
-        except NoSuchJobError:
-            return {'code': StatesCode.JOB_NOT_EXIST, 'message': '任务不存在！'}
-        except Exception as e:
-            traceback.print_exc()
-            return {'code': StatesCode.UNKNOWN_ERROR, 'message': f'位置错误：{str(e)}'}
-
         return {'code': 0, 'data': job_id}
